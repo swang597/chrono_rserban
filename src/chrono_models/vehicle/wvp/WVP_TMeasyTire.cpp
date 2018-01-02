c4eeb44037a2cd7675eb6b16cabb022820511ccf
Rainer Gericke <dr.ratzelbrumm@gmail.com>
2018-01-02 19:11:38 +0100

    TMeasy tire parameters are taken from Pacejka89 as close as possible instead of truck pattern.

--- src/chrono_models/vehicle/wvp/WVP_TMeasyTire.cpp
+++ src/chrono_models/vehicle/wvp/WVP_TMeasyTire.cpp
@@ -39,8 +39,7 @@ const ChVector<> WVP_TMeasyTire::m_inertia(9.62, 16.84, 9.62);
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 WVP_TMeasyTire::WVP_TMeasyTire(const std::string& name) : ChTMeasyTire(name) {
-	// Set Handling Charecteristics
-    SetTMeasyParams();
+	// Set Handling Charecteristics as close as possible to Pacejka89 model data
     
     // Set nonlinear vertical stiffness
     std::vector<double> disp, force;
@@ -56,6 +55,47 @@ WVP_TMeasyTire::WVP_TMeasyTire(const std::string& name) : ChTMeasyTire(name) {
     disp.push_back(0.10); force.push_back(40772.0);
     
     VerticalStiffnessByTable(disp,force);
+    
+    double xi = 0.05;                  // tire damping ratio
+
+    m_width 		  	 = 0.372;
+    m_unloaded_radius 	 = 1.096 / 2.0;
+    m_rolling_resistance = 0.015;
+    m_TMeasyCoeff.mu_0 	 = 0.8;
+    
+    // average tire vertical spring rate
+    double defl = (-m_TMeasyCoeff.cz+sqrt(pow(m_TMeasyCoeff.cz,2)
+    	+4.0*m_TMeasyCoeff.czq*m_TMeasyCoeff.pn))/(2.0*m_TMeasyCoeff.czq);
+	double czm = m_TMeasyCoeff.cz+2.0*m_TMeasyCoeff.czq*defl;
+	m_TMeasyCoeff.dz = 2.0*xi*sqrt(czm*WVP_TMeasyTire::m_mass);
+	
+	m_TMeasyCoeff.dfx0_pn    =  208983.611609;
+	m_TMeasyCoeff.sxm_pn     =  0.104000;
+	m_TMeasyCoeff.fxm_pn     =  13832.621098;
+	m_TMeasyCoeff.sxs_pn     =  0.500000;
+	m_TMeasyCoeff.fxs_pn     =  12541.768777;
+	m_TMeasyCoeff.dfx0_p2n   =  442370.170045;
+	m_TMeasyCoeff.sxm_p2n    =  0.064000;
+	m_TMeasyCoeff.fxm_p2n    =  24576.287112;
+	m_TMeasyCoeff.sxs_p2n    =  0.800000;
+	m_TMeasyCoeff.fxs_p2n    =  22495.052482;
+	m_TMeasyCoeff.dfy0_pn    =  167824.039124;
+	m_TMeasyCoeff.sym_pn     =  0.385868;
+	m_TMeasyCoeff.fym_pn     =  13201.861871;
+	m_TMeasyCoeff.sys_pn     =  0.800000;
+	m_TMeasyCoeff.fys_pn     =  12541.768777;
+	m_TMeasyCoeff.dfy0_p2n   =  276247.933241;
+	m_TMeasyCoeff.sym_p2n    =  0.275446;
+	m_TMeasyCoeff.fym_p2n    =  23679.002612;
+	m_TMeasyCoeff.sys_p2n    =  1.000000;
+	m_TMeasyCoeff.fys_p2n    =  22495.052482;
+	m_TMeasyCoeff.nto0_pn    =  0.160000;
+	m_TMeasyCoeff.synto0_pn  =  0.200000;
+	m_TMeasyCoeff.syntoE_pn  =  0.480000;
+	m_TMeasyCoeff.nto0_p2n   =  0.170000;
+	m_TMeasyCoeff.synto0_p2n =  0.200000;
+	m_TMeasyCoeff.syntoE_p2n =  0.500000;
+
 }
 
 // -----------------------------------------------------------------------------
