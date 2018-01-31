4869950c0b17c9565aca7f22934694b95f59e72b
Radu Serban <serban@wisc.edu>
2018-01-31 10:58:00 +0100

    UpdateWVP drawbar pull test to calculate estimated draw bar pull and tractive effort

--- src/tests/wvp_benchmark/sequential/test_WVP_drawbar_pull.cpp
+++ src/tests/wvp_benchmark/sequential/test_WVP_drawbar_pull.cpp
@@ -21,7 +21,9 @@
 // =============================================================================
 
 #include <cmath>
+#include <numeric>
 
+#include "chrono/utils/ChFilters.h"
 #include "chrono/core/ChFileutils.h"
 #include "chrono/motion_functions/ChFunction.h"
 #include "chrono/physics/ChLinkMotorLinearSpeed.h"
@@ -34,6 +36,8 @@
 
 #include "chrono_models/vehicle/wvp/WVP.h"
 
+////#define MAC_PATH_HACK
+
 #define USE_IRRLICHT
 
 using namespace chrono;
@@ -43,33 +47,61 @@ using namespace chrono::vehicle::wvp;
 // =============================================================================
 
 // Initial vehicle location and orientation
-double terrainLength = 30;  /// 400;
+double terrainLength = 30;
 ChVector<> initLoc(-terrainLength / 2 + 6, 0, 0.7);
 
-// Simulation step size
+// Simulation step size [s]
 double step_size = 1e-4;
 
-// Simulation end time
-double tend = 8;
+// Simulation end time [s]
+double tend = 10;
 
-// Time interval between two render frames
+// Imposed forward speed [m/s]
+double mph2ms = 0.44704;
+double vel = 5 * mph2ms;
+////double vel = 1;
+
+// Default longitudinal slip (can be specified as a program argument)
+double slip = 0.0;
+
+// Time interval between two output frames [s]
 double output_step_size = 1.0 / 400;
 
-// output directory
+// Window size for running average [s]
+double window_size = 0.5;
+
+// Start recording drawbar pull and motor torques after an initial duration [s]
+double skip_time = 0.3;
+
+// Output directory
 const std::string out_dir = "../WVP_DRAWBAR_PULL";
 bool output = true;
 
 // =============================================================================
 
+// Calculate longitudinal slip from radius, chassis velocity, and wheel angular velocity
+double calcLongSlip(double r, double v, double w) {
+    return 1 - v / (r * w);
+}
+
+// =============================================================================
+
 int main(int argc, char* argv[]) {
     // Longitudinal slip value
-    double slip = 0.0;
     if (argc > 1) {
         slip = atof(argv[1]);
     }
-
-    // Imposed vehicle forward speed
-    double vel = 1;
+    
+#ifdef MAC_PATH_HACK
+    // fit data paths to MacOS
+    std::string chrono_dir = "";
+    std::string vehicle_dir = "";
+    chrono_dir += getenv("HOME");
+    chrono_dir += "/chrono-posttest/data/";
+    vehicle_dir = chrono_dir + "vehicle/";
+    SetChronoDataPath(chrono_dir);
+    SetDataPath(vehicle_dir);
+#endif
 
     // --------------
     // Create vehicle
@@ -92,13 +124,18 @@ int main(int argc, char* argv[]) {
     wvp.SetWheelVisualizationType(VisualizationType::NONE);
     wvp.SetTireVisualizationType(VisualizationType::PRIMITIVES);
 
+    double radius = wvp.GetTire(FRONT_LEFT)->GetRadius();
+
     std::cout << "Vehicle mass:               " << wvp.GetVehicle().GetVehicleMass() << std::endl;
     std::cout << "Vehicle mass (with tires):  " << wvp.GetTotalMass() << std::endl;
-    std::cout << "Tire radius:                " << wvp.GetTire(FRONT_LEFT)->GetRadius() << std::endl;
+    std::cout << "Tire radius:                " << radius << std::endl;
 
     // ---------------
     // Create test rig
     // ---------------
+
+    // Locate the rig body approximately at the wheel center height
+    ChVector<> rigLoc(initLoc.x(), initLoc.y(), radius);
 
     auto ground = std::shared_ptr<ChBody>(wvp.GetSystem()->NewBody());
     ground->SetBodyFixed(true);
@@ -107,15 +144,15 @@ int main(int argc, char* argv[]) {
     {
         auto cyl = std::make_shared<ChCylinderShape>();
         cyl->GetCylinderGeometry().rad = 0.02;
-        cyl->GetCylinderGeometry().p1 = ChVector<>(-terrainLength / 2, 0, initLoc.z());
-        cyl->GetCylinderGeometry().p2 = ChVector<>(+terrainLength / 2, 0, initLoc.z());
+        cyl->GetCylinderGeometry().p1 = ChVector<>(-terrainLength / 2, 0, rigLoc.z());
+        cyl->GetCylinderGeometry().p2 = ChVector<>(+terrainLength / 2, 0, rigLoc.z());
         ground->AddAsset(cyl);
         auto col = std::make_shared<ChColorAsset>(0.2f, 0.3f, 0.6f);
         ground->AddAsset(col);
     }
 
     auto rig = std::shared_ptr<ChBody>(wvp.GetSystem()->NewBody());
-    rig->SetPos(initLoc);
+    rig->SetPos(rigLoc);
     rig->SetRot(QUNIT);
     rig->SetMass(100);
     rig->SetBodyFixed(false);
@@ -128,11 +165,11 @@ int main(int argc, char* argv[]) {
     }
 
     auto prismatic1 = std::make_shared<ChLinkLockPrismatic>();
-    prismatic1->Initialize(ground, rig, ChCoordsys<>(initLoc, Q_from_AngY(CH_C_PI_2)));
+    prismatic1->Initialize(ground, rig, ChCoordsys<>(rigLoc, Q_from_AngY(CH_C_PI_2)));
     wvp.GetSystem()->AddLink(prismatic1);
 
     auto prismatic2 = std::make_shared<ChLinkLockPrismatic>();
-    prismatic2->Initialize(rig, wvp.GetChassisBody(), ChCoordsys<>(initLoc, QUNIT));
+    prismatic2->Initialize(rig, wvp.GetChassisBody(), ChCoordsys<>(rigLoc, QUNIT));
     wvp.GetSystem()->AddLink(prismatic2);
 
     // ----------------
@@ -140,7 +177,6 @@ int main(int argc, char* argv[]) {
     // ----------------
 
     // Calculate wheel angular velocity for given slip
-    double radius = wvp.GetTire(FRONT_LEFT)->GetRadius();
     double omega = vel / (radius * (1 - slip));
 
     std::cout << "Slip: " << slip << "  Forward vel: " << vel << "  Wheel omg: " << omega << std::endl;
@@ -148,7 +184,7 @@ int main(int argc, char* argv[]) {
     // Impose forward vehicle velocity
     ////auto fun_vel = std::make_shared<ChFunction_Const>(vel);
     ////auto actuator = std::make_shared<ChLinkMotorLinearSpeed>();
-    ////actuator->Initialize(rig, ground, ChFrame<>(initLoc, QUNIT));
+    ////actuator->Initialize(rig, ground, ChFrame<>(rigLoc, QUNIT));
     ////actuator->SetSpeedFunction(fun_vel);
     ////wvp.GetSystem()->AddLink(actuator);
 
@@ -156,7 +192,7 @@ int main(int argc, char* argv[]) {
     auto actuator = std::make_shared<ChLinkLinActuator>();
     actuator->Set_lin_offset(1);
     actuator->Set_dist_funct(fun_vel);
-    actuator->Initialize(ground, rig, false, ChCoordsys<>(initLoc, QUNIT), ChCoordsys<>(initLoc + ChVector<>(1, 0, 0), QUNIT));
+    actuator->Initialize(ground, rig, false, ChCoordsys<>(rigLoc, QUNIT), ChCoordsys<>(rigLoc + ChVector<>(1, 0, 0), QUNIT));
     wvp.GetSystem()->AddLink(actuator);
 
     // Impose wheel angular velocity
@@ -230,7 +266,13 @@ int main(int argc, char* argv[]) {
     csv.stream().precision(6);
 
     // Number of simulation steps between two output frames
-    int output_steps = (int)std::ceil(output_step_size / step_size);
+    int output_steps = static_cast<int>(std::ceil(output_step_size / step_size));
+
+    // Running average filters for reaction force and torques
+    int filter_steps = static_cast<int>(window_size / output_step_size);
+    utils::ChRunningAverage avg_force(filter_steps);
+    utils::ChRunningAverage avg_torque1(filter_steps), avg_torque2(filter_steps);
+    utils::ChRunningAverage avg_torque3(filter_steps), avg_torque4(filter_steps);
 
     // ---------------
     // Simulation loop
@@ -240,7 +282,10 @@ int main(int argc, char* argv[]) {
     double time = 0;
     double total_timing = 0;
 
-    while (wvp.GetSystem()->GetChTime() < tend) {
+    std::vector<double> drawbarpull;
+    std::vector<double> tractivetorque;
+
+    while (time < tend) {
         if (wvp.GetVehicle().GetVehiclePos().x() > terrainLength / 2 - 1)
             break;
 
@@ -275,14 +320,33 @@ int main(int argc, char* argv[]) {
         app.Advance(step_size);
 #endif
 
+        // Extract simulation outputs
         if (output && step_number % output_steps == 0) {
-            csv << time;
-            csv << wvp.GetVehicle().GetVehicleSpeed();
-            for (int i = 0; i < 4; i++) {
-                csv << wvp.GetVehicle().GetWheelOmega(i);
-                csv << wvp.GetTire(i)->GetLongitudinalSlip();
+            double force = actuator->Get_react_force().x();  // forward speed actuator force
+            std::vector<double> torques = {
+                motors[0]->GetMotorTorque(),  // wheel motor torque front-left
+                motors[1]->GetMotorTorque(),  // wheel motor torque front-right
+                motors[2]->GetMotorTorque(),  // wheel motor torque rear-left
+                motors[3]->GetMotorTorque()   // wheel motor torque rear-right
+            };
+
+            // Collect filtered reaction force and torques
+            if (time >= skip_time) {
+                drawbarpull.push_back(avg_force.Add(force));
+                tractivetorque.push_back(avg_torque1.Add(torques[0]) + avg_torque2.Add(torques[1]) +
+                                         avg_torque3.Add(torques[2]) + avg_torque4.Add(torques[3]));
             }
-            csv << actuator->Get_react_force();
+
+            // Record raw data
+            double actual_v = wvp.GetVehicle().GetVehicleSpeed();
+            csv << time;
+            csv << actual_v;
+            for (int i = 0; i < 4; i++) {
+                double actual_omega = -wvp.GetVehicle().GetWheelOmega(i);
+                double actual_slip = 1 - actual_v / (radius * actual_omega);
+                csv << actual_omega << actual_slip << torques[i];
+            }
+            csv << force;
             csv << std::endl;
         }
 
@@ -292,11 +356,29 @@ int main(int argc, char* argv[]) {
         total_timing += wvp.GetSystem()->GetTimerStep();
     }
 
+    std::cout << "\nSimulation time: " << time << std::endl;
     std::cout << "Total execution time: " << total_timing << std::endl;
 
     if (output) {
         csv.write_to_file(out_dir + "/output_" + std::to_string(slip) + ".dat");
     }
+
+    // Estimated drawbar pull and tractive effort
+    double drawbarpull_avg = 0;
+    double tractivetorque_avg = 0;
+    if (drawbarpull.size() > 0) {
+        drawbarpull_avg = std::accumulate(drawbarpull.begin(), drawbarpull.end(), 0.0) / drawbarpull.size();
+        tractivetorque_avg = std::accumulate(tractivetorque.begin(), tractivetorque.end(), 0.0) / tractivetorque.size();
+    } else {
+        std::cout << "Attention: Insufficient data. Data collection starts after t = " << skip_time << std::endl;
+    }
+    double tractiveeffort_avg = tractivetorque_avg / radius;
+
+    std::cout << "Slip: " << slip << "\tDrawbar pull: " << drawbarpull_avg
+              << "\tTractive effort: " << tractiveeffort_avg << std::endl;
+
+    std::ofstream res(out_dir + "/results.dat", std::ofstream::app);
+    res << slip << "\t" << drawbarpull_avg << "\t" << tractiveeffort_avg << std::endl;
 
     return 0;
 }
