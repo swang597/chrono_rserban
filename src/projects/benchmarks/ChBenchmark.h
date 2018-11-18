0ad1e0ee9db1a1adb4ca1b835d687bf7cb751cce
Radu Serban <serban@wisc.edu>
2018-11-18 14:22:40 +0100

    Include time for overall collision detection in benchmark output

--- src/projects/benchmarks/ChBenchmark.h
+++ src/projects/benchmarks/ChBenchmark.h
@@ -34,6 +34,7 @@ class ChBenchmarkTest {
 
     double m_timer_step;
     double m_timer_setup;
+    double m_timer_collision;
     double m_timer_collision_broad;
     double m_timer_collision_narrow;
     double m_timer_solver;
@@ -46,6 +47,7 @@ class ChBenchmarkTest {
 inline ChBenchmarkTest::ChBenchmarkTest()
     : m_timer_step(0),
       m_timer_setup(0),
+      m_timer_collision(0),
       m_timer_collision_broad(0),
       m_timer_collision_narrow(0),
       m_timer_solver(0),
@@ -57,6 +59,7 @@ inline void ChBenchmarkTest::Simulate(int num_steps) {
         ExecuteStep();
         m_timer_step += GetSystem()->GetTimerStep();
         m_timer_setup += GetSystem()->GetTimerSetup();
+        m_timer_collision += GetSystem()->GetTimerCollision();
         m_timer_collision_broad += GetSystem()->GetTimerCollisionBroad();
         m_timer_collision_narrow += GetSystem()->GetTimerCollisionNarrow();
         m_timer_solver += GetSystem()->GetTimerSolver();
@@ -67,6 +70,7 @@ inline void ChBenchmarkTest::Simulate(int num_steps) {
 inline void ChBenchmarkTest::ResetTimers() {
     m_timer_step = 0;
     m_timer_setup = 0;
+    m_timer_collision = 0;
     m_timer_collision_broad = 0;
     m_timer_collision_narrow = 0;
     m_timer_solver = 0;
@@ -91,6 +95,7 @@ class ChBenchmarkFixture : public ::benchmark::Fixture {
         st.counters["Setup"] = m_test->m_timer_setup * 1e3;
         st.counters["Solve"] = m_test->m_timer_solver * 1e3;
         st.counters["Update"] = m_test->m_timer_update * 1e3;
+        st.counters["Collision"] = m_test->m_timer_collision * 1e3;
         st.counters["Broad"] = m_test->m_timer_collision_broad * 1e3;
         st.counters["Narrow"] = m_test->m_timer_collision_narrow * 1e3;
     }
