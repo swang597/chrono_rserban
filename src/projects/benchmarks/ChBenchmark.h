5fbbdb234fc60057cc8d301292805a72e86c5f5d
Radu Serban <serban@wisc.edu>
2018-11-21 23:52:37 +0100

    Report more detailed timers for benchmarks

--- src/projects/benchmarks/ChBenchmark.h
+++ src/projects/benchmarks/ChBenchmark.h
@@ -33,11 +33,13 @@ class ChBenchmarkTest {
     void ResetTimers();
 
     double m_timer_step;
+    double m_timer_advance;
+    double m_timer_jacobian;
     double m_timer_setup;
+    double m_timer_solver;
     double m_timer_collision;
     double m_timer_collision_broad;
     double m_timer_collision_narrow;
-    double m_timer_solver;
     double m_timer_update;
 
   protected:
@@ -46,11 +48,13 @@ class ChBenchmarkTest {
 
 inline ChBenchmarkTest::ChBenchmarkTest()
     : m_timer_step(0),
+      m_timer_advance(0),
+      m_timer_jacobian(0),
       m_timer_setup(0),
+      m_timer_solver(0),
       m_timer_collision(0),
       m_timer_collision_broad(0),
       m_timer_collision_narrow(0),
-      m_timer_solver(0),
       m_timer_update(0) {}
 
 inline void ChBenchmarkTest::Simulate(int num_steps) {
@@ -58,22 +62,26 @@ inline void ChBenchmarkTest::Simulate(int num_steps) {
     for (int i = 0; i < num_steps; i++) {
         ExecuteStep();
         m_timer_step += GetSystem()->GetTimerStep();
+        m_timer_advance += GetSystem()->GetTimerAdvance();
+        m_timer_jacobian += GetSystem()->GetTimerJacobian();
         m_timer_setup += GetSystem()->GetTimerSetup();
+        m_timer_solver += GetSystem()->GetTimerSolver();
         m_timer_collision += GetSystem()->GetTimerCollision();
         m_timer_collision_broad += GetSystem()->GetTimerCollisionBroad();
         m_timer_collision_narrow += GetSystem()->GetTimerCollisionNarrow();
-        m_timer_solver += GetSystem()->GetTimerSolver();
         m_timer_update += GetSystem()->GetTimerUpdate();
     }
 }
 
 inline void ChBenchmarkTest::ResetTimers() {
     m_timer_step = 0;
+    m_timer_advance = 0;
+    m_timer_jacobian = 0;
     m_timer_setup = 0;
+    m_timer_solver = 0;
     m_timer_collision = 0;
     m_timer_collision_broad = 0;
     m_timer_collision_narrow = 0;
-    m_timer_solver = 0;
     m_timer_update = 0;
 }
 
@@ -91,13 +99,15 @@ class ChBenchmarkFixture : public ::benchmark::Fixture {
     ~ChBenchmarkFixture() { delete m_test; }
 
     void Report(benchmark::State& st) {
-        st.counters["Step"] = m_test->m_timer_step * 1e3;
-        st.counters["Setup"] = m_test->m_timer_setup * 1e3;
-        st.counters["Solve"] = m_test->m_timer_solver * 1e3;
-        st.counters["Update"] = m_test->m_timer_update * 1e3;
-        st.counters["Collision"] = m_test->m_timer_collision * 1e3;
-        st.counters["Broad"] = m_test->m_timer_collision_broad * 1e3;
-        st.counters["Narrow"] = m_test->m_timer_collision_narrow * 1e3;
+        st.counters["Step_Total"] = m_test->m_timer_step * 1e3;
+        st.counters["Step_Advance"] = m_test->m_timer_advance * 1e3;
+        st.counters["Step_Update"] = m_test->m_timer_update * 1e3;
+        st.counters["LS_Jacobian"] = m_test->m_timer_jacobian * 1e3;
+        st.counters["LS_Setup"] = m_test->m_timer_setup * 1e3;
+        st.counters["LS_Solve"] = m_test->m_timer_solver * 1e3;
+        st.counters["CD_Total"] = m_test->m_timer_collision * 1e3;
+        st.counters["CD_Broad"] = m_test->m_timer_collision_broad * 1e3;
+        st.counters["CD_Narrow"] = m_test->m_timer_collision_narrow * 1e3;
     }
 
     TEST* m_test;
