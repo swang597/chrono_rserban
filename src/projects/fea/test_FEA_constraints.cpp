1c1a00ca5b2c0243ceb2a3501626c8873f23f15e
Radu Serban <serban@wisc.edu>
2018-12-18 21:35:38 +0100

    Add test for disabling FEA constraints during simulation

---
+++ src/projects/fea/test_FEA_constraints.cpp
@@ -0,0 +1,207 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Test for disabling mesh constraints
+//
+// =============================================================================
+
+#include "chrono/physics/ChSystemSMC.h"
+#include "chrono/solver/ChSolverMINRES.h"
+
+#include "chrono_irrlicht/ChIrrApp.h"
+
+#include "chrono_mkl/ChSolverMKL.h"
+
+#include "chrono/fea/ChBuilderBeam.h"
+#include "chrono/fea/ChElementCableANCF.h"
+#include "chrono/fea/ChLinkDirFrame.h"
+#include "chrono/fea/ChLinkPointFrame.h"
+#include "chrono/fea/ChMesh.h"
+#include "chrono/fea/ChVisualizationFEAmesh.h"
+
+using namespace chrono;
+
+// Define FIX_NODES to fix mesh nodes.
+// Otherwise, use constraints.
+////#define FIX_NODES
+
+int main(int argc, char* argv[]) {
+    // Settings
+    bool use_MKL = false;
+    bool use_HHT = false;
+
+    ChSystemSMC sys;
+    sys.Set_G_acc(ChVector<>(-1.0, 0.0, 0.0));
+    ////sys.Set_G_acc(ChVector<>(0.0, -1.0, 0.0));
+    ////sys.Set_G_acc(ChVector<>(0.0, 0.0, -1.0));
+    ////sys.Set_G_acc(ChVector<>(0.0, 0.0, 0.0));
+
+    // Ground body
+    auto ground = std::make_shared<ChBody>();
+    ground->SetIdentifier(-1);
+    ground->SetBodyFixed(true);
+    sys.Add(ground);
+
+    auto mesh = std::make_shared<fea::ChMesh>();
+
+    auto section = std::make_shared<fea::ChBeamSectionCable>();
+    section->SetDiameter(0.002);
+    section->SetYoungModulus(1e6);
+    section->SetDensity(2000);
+    section->SetBeamRaleyghDamping(0.8);
+
+    std::vector<std::shared_ptr<fea::ChNodeFEAxyzD>> base_nodes;
+    std::vector<std::shared_ptr<fea::ChNodeFEAxyzD>> tip_nodes;
+
+    for (int j = 0; j < 1; ++j) {
+        fea::ChBuilderBeamANCF builder;
+        builder.BuildBeam(mesh,                            //
+                          section,                         //
+                          4,                               //
+                          ChVector<>(0.0, 0.0, -0.1 * j),  // base location
+                          ChVector<>(0.0, 0.1, -0.1 * j)   // tip location
+        );
+
+        ////builder.GetLastBeamNodes().back()->SetForce(ChVector<>(-0.1, 0, 0));
+
+        base_nodes.push_back(builder.GetLastBeamNodes().front());
+        tip_nodes.push_back(builder.GetLastBeamNodes().back());
+    }
+
+#ifdef FIX_NODES
+
+    for (auto node : base_nodes)
+        node->SetFixed(true);
+
+    for (auto node : tip_nodes)
+        node->SetFixed(true);
+
+#else
+
+    std::vector<std::shared_ptr<ChLinkBase>> base_cnstr;
+    std::vector<std::shared_ptr<ChLinkBase>> tip_cnstr;
+
+    for (auto node : base_nodes) {
+        auto pos_cnstr = std::make_shared<fea::ChLinkPointFrame>();
+        pos_cnstr->Initialize(node, ground);
+        sys.Add(pos_cnstr);
+
+        auto dir_cnstr = std::make_shared<fea::ChLinkDirFrame>();
+        dir_cnstr->Initialize(node, ground);
+        dir_cnstr->SetDirectionInAbsoluteCoords(node->D);
+        sys.Add(dir_cnstr);
+
+        base_cnstr.push_back(pos_cnstr);
+        base_cnstr.push_back(dir_cnstr);
+    }
+
+    for (auto node : tip_nodes) {
+        auto pos_cnstr = std::make_shared<fea::ChLinkPointFrame>();
+        pos_cnstr->Initialize(node, ground);
+        sys.Add(pos_cnstr);
+
+        auto dir_cnstr = std::make_shared<fea::ChLinkDirFrame>();
+        dir_cnstr->Initialize(node, ground);
+        dir_cnstr->SetDirectionInAbsoluteCoords(node->D);
+        sys.Add(dir_cnstr);
+
+        tip_cnstr.push_back(pos_cnstr);
+        tip_cnstr.push_back(dir_cnstr);
+    }
+
+#endif
+
+    sys.Add(mesh);
+
+    auto mvisA = std::make_shared<fea::ChVisualizationFEAmesh>(*mesh);
+    mvisA->SetFEMdataType(fea::ChVisualizationFEAmesh::E_PLOT_ANCF_BEAM_BD);
+    mvisA->SetColorscaleMinMax(-02, 20);
+    mvisA->SetSmoothFaces(true);
+    mvisA->SetWireframe(false);
+    mesh->AddAsset(mvisA);
+
+    auto mvisB = std::make_shared<fea::ChVisualizationFEAmesh>(*mesh);
+    mvisB->SetFEMglyphType(fea::ChVisualizationFEAmesh::E_GLYPH_NODE_DOT_POS);
+    mvisB->SetFEMdataType(fea::ChVisualizationFEAmesh::E_PLOT_NONE);
+    mvisB->SetSymbolsThickness(0.004);
+    mvisB->SetSymbolsScale(0.002);
+    mvisB->SetZbufferHide(false);
+    mesh->AddAsset(mvisB);
+
+    // Mark completion of system construction
+    sys.SetupInitial();
+
+    // Run-time visualization
+    irrlicht::ChIrrApp application(&sys, L"Cables FEM", irr::core::dimension2d<irr::u32>(800, 600), false, true);
+    application.AddTypicalLogo();
+    application.AddTypicalSky();
+    application.AddTypicalLights();
+    application.AddTypicalCamera(irr::core::vector3df(0.0f, 0.05f, 0.2f));
+
+    application.AssetBindAll();
+    application.AssetUpdateAll();
+
+    // Solver settings
+    if (use_MKL) {
+        auto mkl_solver = std::make_shared<ChSolverMKL<>>();
+        mkl_solver->SetSparsityPatternLock(true);
+        sys.SetSolver(mkl_solver);
+    } else {
+        sys.SetSolverType(ChSolver::Type::MINRES);
+        sys.SetSolverWarmStarting(true);
+        sys.SetTolForce(1e-10);
+        sys.SetMaxItersSolverSpeed(10000);
+        sys.SetMaxItersSolverStab(10000);
+    }
+
+    // Integrator settings
+    // -------------------
+    if (use_HHT) {
+        sys.SetTimestepperType(ChTimestepper::Type::HHT);
+        auto stepper = std::dynamic_pointer_cast<ChTimestepperHHT>(sys.GetTimestepper());
+        stepper->SetAlpha(-0.1);
+        stepper->SetMaxiters(100);
+        stepper->SetAbsTolerances(1e-10, 1e-10);
+        stepper->SetMode(ChTimestepperHHT::ACCELERATION);
+        stepper->SetScaling(true);
+        stepper->SetVerbose(false);
+    } else {
+        sys.SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT);
+        ////sys.SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT_LINEARIZED);
+    }
+
+    // Simulation loop
+    application.SetTimestep(0.01);
+
+    bool disabled = false;
+    while (application.GetDevice()->run()) {
+        if (!disabled && sys.GetChTime() > 2) {
+#ifdef FIX_NODES
+            for (auto node : tip_nodes)
+                node->SetFixed(false);
+#else
+            for (auto cnstr : tip_cnstr)
+                cnstr->SetDisabled(true);
+#endif
+            disabled = true;
+        }
+
+        application.BeginScene();
+        application.DrawAll();
+        application.DoStep();
+        application.EndScene();
+    }
+
+    return 0;
+}
