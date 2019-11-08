bac5fce450f8fd42c08dbfb021cda022ece6b144
Radu Serban <serban@wisc.edu>
2019-11-08 13:15:35 +0100

    Modify test_CTC_mesh to check both mesh-mesh and mesh-box collision

--- src/projects/contact/sequential/test_CTC_mesh.cpp
+++ src/projects/contact/sequential/test_CTC_mesh.cpp
@@ -35,6 +35,14 @@ int main(int argc, char* argv[]) {
     double gravity = 9.81;    // gravitational acceleration
     double time_step = 1e-4;  // integration step size
 
+    enum class ContactType {
+        MESH_BOX,  // use box for ground collision model
+        MESH_MESH  // use mesh for ground collision model
+    };
+    ContactType contact_type = ContactType::MESH_MESH;
+
+    double mesh_swept_sphere_radius = 0.005; 
+
     // ---------------------------
     // Contact material properties
     // ---------------------------
@@ -53,7 +61,7 @@ int main(int argc, char* argv[]) {
 
     float ground_friction = 0.9f;
     float ground_restitution = 0.01f;
-    float ground_young_modulus = 2e7f;
+    float ground_young_modulus = 1e6f;
     float ground_poisson_ratio = 0.3f;
     float ground_adhesion = 0.0f;
     float ground_kn = 2e5;
@@ -65,10 +73,7 @@ int main(int argc, char* argv[]) {
     // Parameters for the falling object
     // ---------------------------------
 
-    int objectId = 100;
-    double mass = 1000;
-    ChVector<> pos(0, 0, 1);
-    ChQuaternion<> rot(1, 0, 0, 0);
+    ChVector<> pos(0, 0.75, 0);
     ChVector<> init_vel(0, 0, 0);
     ChVector<> init_omg(0, 0, 0);
 
@@ -76,7 +81,6 @@ int main(int argc, char* argv[]) {
     // Parameters for the containing bin
     // ---------------------------------
 
-    int groundId = 200;
     double width = 2;
     double length = 1;
     double thickness = 0.1;
@@ -96,7 +100,7 @@ int main(int argc, char* argv[]) {
             break;
     }
 
-    system->Set_G_acc(ChVector<>(0, 0, -gravity));
+    system->Set_G_acc(ChVector<>(0, -gravity, 0));
 
     // Create the Irrlicht visualization
     ChIrrApp application(system, L"mesh collision", irr::core::dimension2d<irr::u32>(800, 600), false, true);
@@ -105,21 +109,23 @@ int main(int argc, char* argv[]) {
     application.AddTypicalLogo();
     application.AddTypicalSky();
     application.AddTypicalLights();
-    application.AddTypicalCamera(irr::core::vector3df(0, 4, -0.2f), irr::core::vector3df(0, 0, 0));
+    application.AddTypicalCamera(irr::core::vector3df(0.5, 1, -2), irr::core::vector3df(0, 0, 0));
 
     // This means that contact forces will be shown in Irrlicht application
-    application.SetSymbolscale(1e-4);
+    application.SetSymbolscale(5e-3);
     application.SetContactsDrawMode(ChIrrTools::eCh_ContactsDrawMode::CONTACT_FORCES);
+
+    // Rotation Z->Y (becauset meshes used here assume Z up)
+    ChQuaternion<> z2y = Q_from_AngX(-CH_C_PI_2);
 
     // Create the falling object
     auto object = std::shared_ptr<ChBody>(system->NewBody());
     system->AddBody(object);
 
-    object->SetIdentifier(objectId);
-    object->SetMass(mass);
-    object->SetInertiaXX(400.0 * ChVector<>(1, 1, 1));
+    object->SetMass(200);
+    object->SetInertiaXX(40.0 * ChVector<>(1, 1, 0.2));
     object->SetPos(pos);
-    object->SetRot(rot);
+    object->SetRot(z2y);
     object->SetPos_dt(init_vel);
     object->SetWvel_par(init_omg);
     object->SetCollide(true);
@@ -146,7 +152,8 @@ int main(int argc, char* argv[]) {
     trimesh->LoadWavefrontMesh(GetChronoDataFile("vehicle/hmmwv/hmmwv_tire.obj"), true, false);
 
     object->GetCollisionModel()->ClearModel();
-    object->GetCollisionModel()->AddTriangleMesh(trimesh, false, false, ChVector<>(0), ChMatrix33<>(1), 0.01);
+    object->GetCollisionModel()->AddTriangleMesh(trimesh, false, false, ChVector<>(0), ChMatrix33<>(1),
+                                                 mesh_swept_sphere_radius);
     object->GetCollisionModel()->BuildModel();
 
     auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
@@ -161,10 +168,9 @@ int main(int argc, char* argv[]) {
     auto ground = std::shared_ptr<ChBody>(system->NewBody());
     system->AddBody(ground);
 
-    ground->SetIdentifier(groundId);
     ground->SetMass(1);
     ground->SetPos(ChVector<>(0, 0, 0));
-    ground->SetRot(ChQuaternion<>(1, 0, 0, 0));
+    ground->SetRot(z2y);
     ground->SetCollide(true);
     ground->SetBodyFixed(true);
 
@@ -185,14 +191,35 @@ int main(int argc, char* argv[]) {
             break;
     }
 
-    ground->GetCollisionModel()->ClearModel();
-    ground->GetCollisionModel()->AddBox(width, length, thickness, ChVector<>(0, 0, -thickness));
-    ground->GetCollisionModel()->BuildModel();
+    switch (contact_type) {
+        case ContactType::MESH_BOX: {
+            ground->GetCollisionModel()->ClearModel();
+            ground->GetCollisionModel()->AddBox(width, length, thickness, ChVector<>(0, 0, -thickness));
+            ground->GetCollisionModel()->BuildModel();
 
-    auto box = chrono_types::make_shared<ChBoxShape>();
-    box->GetBoxGeometry().Size = ChVector<>(width, length, thickness);
-    box->GetBoxGeometry().Pos = ChVector<>(0, 0, -thickness);
-    ground->AddAsset(box);
+            auto box = chrono_types::make_shared<ChBoxShape>();
+            box->GetBoxGeometry().Size = ChVector<>(width, length, thickness);
+            box->GetBoxGeometry().Pos = ChVector<>(0, 0, -thickness);
+            ground->AddAsset(box);
+
+            break;
+        }
+        case ContactType::MESH_MESH: {
+            auto trimesh_ground = chrono_types::make_shared<geometry::ChTriangleMeshConnected>();
+            trimesh_ground->LoadWavefrontMesh(GetChronoDataFile("vehicle/terrain/meshes/ramp_10x1.obj"), true, false);
+
+            ground->GetCollisionModel()->ClearModel();
+            ground->GetCollisionModel()->AddTriangleMesh(trimesh_ground, false, false, ChVector<>(0), ChMatrix33<>(1),
+                                                         mesh_swept_sphere_radius);
+            ground->GetCollisionModel()->BuildModel();
+
+            auto trimesh_ground_shape = chrono_types::make_shared<ChTriangleMeshShape>();
+            trimesh_ground_shape->SetMesh(trimesh_ground);
+            ground->AddAsset(trimesh_ground_shape);
+
+            break;
+        }
+    }
 
     // Complete asset construction
     application.AssetBindAll();
