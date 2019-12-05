a121acbdd90c9a1c972eeef068fac484c3dffd6b
Rainer Gericke <rainer.gericke@mac.com>
2019-12-05 07:18:39 +0100

    Power limiter for FED Alpha engine
    The engine seems to be throttled to 90% (information from Jacob Brendle, Tardec). The acceleration test gets more realistic with this change.

--- src/chrono_models/vehicle/feda/FEDA_SimpleMapPowertrain.cpp
+++ src/chrono_models/vehicle/feda/FEDA_SimpleMapPowertrain.cpp
@@ -35,6 +35,8 @@ double FEDA_SimpleMapPowertrain::GetMaxEngineSpeed() {
 }
 
 void FEDA_SimpleMapPowertrain::SetEngineTorqueMaps(ChFunction_Recorder& map0, ChFunction_Recorder& mapF) {
+    const double limit_factor = 0.9;
+
     map0.AddPoint(-10.472, 0.000);
     map0.AddPoint(83.776, 0.0);
 
@@ -60,28 +62,28 @@ void FEDA_SimpleMapPowertrain::SetEngineTorqueMaps(ChFunction_Recorder& map0, Ch
     map0.AddPoint(2525 * rpm2rads, -95.9);
     map0.AddPoint(2850 * rpm2rads, -99.9);
 
-    mapF.AddPoint(-10.472, 300.0);
-    mapF.AddPoint(0, 300.0);
-    mapF.AddPoint(700 * rpm2rads, 400);
-    mapF.AddPoint(800 * rpm2rads, 410);
-    mapF.AddPoint(900 * rpm2rads, 450);
-    mapF.AddPoint(1000 * rpm2rads, 550);
-    mapF.AddPoint(1100 * rpm2rads, 625);
-    mapF.AddPoint(1200 * rpm2rads, 700);
-    mapF.AddPoint(1300 * rpm2rads, 700);
-    mapF.AddPoint(1400 * rpm2rads, 700);
-    mapF.AddPoint(1500 * rpm2rads, 700);
-    mapF.AddPoint(1600 * rpm2rads, 700);
-    mapF.AddPoint(1700 * rpm2rads, 698);
-    mapF.AddPoint(1800 * rpm2rads, 695);
-    mapF.AddPoint(1900 * rpm2rads, 669);
-    mapF.AddPoint(2000 * rpm2rads, 639);
-    mapF.AddPoint(2100 * rpm2rads, 612);
-    mapF.AddPoint(2200 * rpm2rads, 586);
-    mapF.AddPoint(2300 * rpm2rads, 562);
-    mapF.AddPoint(2400 * rpm2rads, 540);
-    mapF.AddPoint(2500 * rpm2rads, 520);
-    mapF.AddPoint(2525 * rpm2rads, 505);
+    mapF.AddPoint(-10.472, 300.0 * limit_factor);
+    mapF.AddPoint(0, 300.0 * limit_factor);
+    mapF.AddPoint(700 * rpm2rads, 400 * limit_factor);
+    mapF.AddPoint(800 * rpm2rads, 410 * limit_factor);
+    mapF.AddPoint(900 * rpm2rads, 450 * limit_factor);
+    mapF.AddPoint(1000 * rpm2rads, 550 * limit_factor);
+    mapF.AddPoint(1100 * rpm2rads, 625 * limit_factor);
+    mapF.AddPoint(1200 * rpm2rads, 700 * limit_factor);
+    mapF.AddPoint(1300 * rpm2rads, 700 * limit_factor);
+    mapF.AddPoint(1400 * rpm2rads, 700 * limit_factor);
+    mapF.AddPoint(1500 * rpm2rads, 700 * limit_factor);
+    mapF.AddPoint(1600 * rpm2rads, 700 * limit_factor);
+    mapF.AddPoint(1700 * rpm2rads, 698 * limit_factor);
+    mapF.AddPoint(1800 * rpm2rads, 695 * limit_factor);
+    mapF.AddPoint(1900 * rpm2rads, 669 * limit_factor);
+    mapF.AddPoint(2000 * rpm2rads, 639 * limit_factor);
+    mapF.AddPoint(2100 * rpm2rads, 612 * limit_factor);
+    mapF.AddPoint(2200 * rpm2rads, 586 * limit_factor);
+    mapF.AddPoint(2300 * rpm2rads, 562 * limit_factor);
+    mapF.AddPoint(2400 * rpm2rads, 540 * limit_factor);
+    mapF.AddPoint(2500 * rpm2rads, 520 * limit_factor);
+    mapF.AddPoint(2525 * rpm2rads, 505 * limit_factor);
     mapF.AddPoint(2550 * rpm2rads, 0);
 }
 
