be762cc6908b8b466ad88b7129cf1f469c91635b
Radu Serban <serban@wisc.edu>
2022-06-23 13:59:47 +0200

    Implement interface to NN torch module

--- src/projects/polaris/sph_nn/test_Polaris_SPHnn.cpp
+++ src/projects/polaris/sph_nn/test_Polaris_SPHnn.cpp
@@ -107,19 +107,22 @@ class NNterrain : public ChTerrain {
     NNterrain(ChSystem& sys, std::shared_ptr<WheeledVehicle> vehicle);
     bool Load(const std::string& pt_file);
     void Create(const std::string& terrain_dir, bool vis = true);
-
-    virtual void Synchronize(double time) override;
+    void Synchronize(double time, const DriverInputs& driver_inputs);
+    virtual void Advance(double step) override;
 
   private:
     ChSystem& m_sys;
     std::shared_ptr<WheeledVehicle> m_vehicle;
     std::array<std::shared_ptr<ChWheel>, 4> m_wheels;
-    std::array<TerrainForce, 4> m_forces;
     std::shared_ptr<ChParticleCloud> m_particles;
+
     ChVector<> m_box_size;
     ChVector<> m_box_offset;
+    std::array<std::vector<ChAparticle*>,4> m_wheel_particles;
 
     torch::jit::script::Module module;
+    std::array<std::vector<ChVector<>>, 4> m_particle_forces;
+    std::array<TerrainForce, 4> m_tire_forces;
 };
 
 NNterrain::NNterrain(ChSystem& sys, std::shared_ptr<WheeledVehicle> vehicle) : m_sys(sys), m_vehicle(vehicle) {
@@ -138,6 +141,11 @@ NNterrain::NNterrain(ChSystem& sys, std::shared_ptr<WheeledVehicle> vehicle) : m
 }
 
 bool NNterrain::Load(const std::string& pt_file) {
+    std::cout << "cuda version " << cuda_version() << std::endl;
+    std::cout << "cuda version " << scatter::cuda_version() << std::endl;
+    torch::Tensor tensor = torch::eye(3);
+    std::cout << tensor << std::endl;
+
     std::ifstream is(pt_file, std::ios_base::binary);
     try {
         // Deserialize the ScriptModule from a file using torch::jit::load().
@@ -186,6 +194,10 @@ void NNterrain::Create(const std::string& terrain_dir, bool vis) {
     }
 
     m_sys.Add(m_particles);
+
+    // Initial size of sampling box particle vectors
+    for (int i = 0; i < 4; i++)
+        m_wheel_particles[i].resize(num_particles);
 }
 
 struct in_box {
@@ -207,32 +219,117 @@ struct in_box {
     ChVector<> h;
 };
 
-void NNterrain::Synchronize(double time) {
-    auto p_all = m_particles->GetParticles();
-    std::array<int, 4> num_particles;
+void NNterrain::Synchronize(double time, const DriverInputs& driver_inputs) {
+    const auto& p_all = m_particles->GetParticles();
+
+    // Prepare NN model inputs
+    std::vector<torch::jit::IValue> inputs;
+
+    // Loop over all vehicle wheels
     for (int i = 0; i < 4; i++) {
         // Wheel state
-        auto wheel_pos = m_wheels[i]->GetPos();
-        auto wheel_normal = m_wheels[i]->GetSpindle()->GetRot().GetYaxis();
+        const auto& w_state = m_wheels[i]->GetState();
+        const auto& w_pos = w_state.pos;
+        const auto& w_rot = w_state.rot;
+        const auto& w_normal = w_rot.GetYaxis();
+        const auto& w_linvel = w_state.lin_vel;
+        const auto& w_angvel = w_state.ang_vel;
         auto tire_radius = m_wheels[i]->GetTire()->GetRadius();
 
         // Sampling OBB
         ChVector<> Z_dir(0, 0, 1);
-        ChVector<> X_dir = Vcross(wheel_normal, ChVector<>(0, 0, 1)).GetNormalized();
+        ChVector<> X_dir = Vcross(w_normal, ChVector<>(0, 0, 1)).GetNormalized();
         ChVector<> Y_dir = Vcross(Z_dir, X_dir);
         ChMatrix33<> box_rot(X_dir, Y_dir, Z_dir);
-        ChVector<> box_pos = wheel_pos + box_rot * (m_box_offset - ChVector<>(0, 0, tire_radius));
+        ChVector<> box_pos = w_pos + box_rot * (m_box_offset - ChVector<>(0, 0, tire_radius));
 
         // Find particles in sampling OBB
-        std::vector<ChAparticle*> p(m_particles->GetNparticles());
-        auto end = std::copy_if(p_all.begin(), p_all.end(), p.begin(), in_box(box_pos, box_rot, m_box_size));
-        size_t num_active = (size_t)(end - p.begin());
-        p.resize(num_active);
+        auto end = std::copy_if(p_all.begin(), p_all.end(), m_wheel_particles[i].begin(), in_box(box_pos, box_rot, m_box_size));
+        size_t num_active = (size_t)(end - m_wheel_particles[i].begin());
+        m_wheel_particles[i].resize(num_active);
 
-        num_particles[i] = num_active;
+        // Load particle positions and velocities
+        auto part_pos = torch::empty(m_wheel_particles[i].size() * 3, torch::kFloat32);
+        auto part_vel = torch::empty(m_wheel_particles[i].size() * 3, torch::kFloat32);
+        float* part_pos_data = part_pos.data<float>();
+        float* part_vel_data = part_vel.data<float>();
+        for (const auto& part : m_wheel_particles[i]) {
+            ChVector<float> p(part->GetPos());
+            *part_pos_data++ = p.x();
+            *part_pos_data++ = p.y();
+            *part_pos_data++ = p.z();
+            ChVector<float> v(part->GetPos_dt());
+            *part_vel_data++ = v.x();
+            *part_vel_data++ = v.y();
+            *part_vel_data++ = v.z();
+        }
+
+        // Load wheel position, orientation, linear velocity, and angular velocity
+        auto w_pos_t = torch::from_blob((void*)(ChVector<float>(w_pos)).data(), {3}, torch::kFloat32);
+        auto w_rot_t = torch::from_blob((void*)(ChQuaternion<float>(w_rot)).data(), {4}, torch::kFloat32);
+        auto w_linvel_t = torch::from_blob((void*)(ChVector<float>(w_linvel)).data(), {3}, torch::kFloat32);
+        auto w_angvel_t = torch::from_blob((void*)(ChVector<float>(w_angvel)).data(), {3}, torch::kFloat32);
+
+        // Prepare the tuple input for this wheel
+        std::vector<torch::jit::IValue> tuple;
+        tuple.push_back(part_pos);
+        tuple.push_back(part_vel);
+        tuple.push_back(w_pos_t);
+        ////tuple.push_back(w_rot_t);
+        tuple.push_back(w_linvel_t);
+        tuple.push_back(w_angvel_t);
+
+        // Add this wheel's tuple to NN model inputs
+        inputs.push_back(torch::ivalue::Tuple::create(tuple));
     }
-    std::copy(num_particles.begin(), num_particles.end(), std::ostream_iterator<int>(std::cout, " "));
-    cout << endl;
+
+    // Load vehicle data (1 tensor)
+    auto drv_inputs = torch::tensor({(float)driver_inputs.m_steering, (float)driver_inputs.m_throttle, (float)driver_inputs.m_braking}, torch::kFloat32);
+
+    // Add the vehicle data to NN model inputs
+    inputs.push_back(drv_inputs);
+
+    // Invoke NN model
+    auto outputs = module.forward(inputs);
+
+    // Loop over all vehicle wheels
+    for (int i = 0; i < 4; i++) {
+        // Outputs for this wheel
+        auto part_frc = outputs.toTuple()->elements()[i].toTensor();
+        auto tire_frc = outputs.toTuple()->elements()[i + 4].toTensor();
+
+        // Extract particle forces
+        size_t num_active = m_wheel_particles[i].size();
+        m_particle_forces[i].resize(num_active);
+        float* part_frc_data = part_frc.data<float>();
+        for (auto& frc : m_particle_forces[i]) {
+            frc = ChVector<>(*part_frc_data++, *part_frc_data++, *part_frc_data++);
+        }
+
+        // Extract tire forces
+        float* tire_frc_data = tire_frc.data<float>();
+        m_tire_forces[i].force = ChVector<>(*tire_frc_data++, *tire_frc_data++, *tire_frc_data++);
+        m_tire_forces[i].moment = ChVector<>(*tire_frc_data++, *tire_frc_data++, *tire_frc_data++);
+        m_tire_forces[i].point = ChVector<>(0, 0, 0);
+    }
+
+    //// TODO - sync with vehicle
+}
+
+void NNterrain::Advance(double step) {
+    // Update state of particles in sampling boxes.
+    // Assume mass=1 for all particles.
+    double step2 = step * step / 2;
+    for (int i = 0; i < 4; i++) {
+        size_t num_active = m_wheel_particles[i].size();
+
+        for (size_t j = 0; j < num_active; j++) {
+            auto v = m_wheel_particles[i][j]->GetPos_dt() + m_particle_forces[i][j] * step; 
+            auto p = m_wheel_particles[i][j]->GetPos() + v * step + m_particle_forces[i][j] * step2;
+            m_wheel_particles[i][j]->SetPos(p);
+            m_wheel_particles[i][j]->SetPos_dt(v);
+        }
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -331,9 +428,7 @@ int main(int argc, char* argv[]) {
     cout << "Create terrain..." << endl;
     NNterrain terrain(sys, vehicle);
     terrain.Create(terrain_dir);
-
-    cout << "Load PT module..." << endl;
-    if (!terrain.Load(vehicle::GetDataFile("terrain/sph/wrapped_gnn_seqlen_1_force.pt"))) {
+    if (!terrain.Load(vehicle::GetDataFile("terrain/sph/wrapped_gnn_seqlen_1_force_cpu.pt"))) {
         return 1;
     }
 
@@ -382,12 +477,14 @@ int main(int argc, char* argv[]) {
                  << driver_inputs.m_throttle << " " << driver_inputs.m_braking << "  spd = " << vehicle->GetSpeed()
                  << endl;
 
+        // Synchronize subsystems
         driver.Synchronize(t);
         vehicle->Synchronize(t, driver_inputs, terrain);
-        terrain.Synchronize(t);
+        terrain.Synchronize(t, driver_inputs);
 
         // Advance system state
         driver.Advance(step_size);
+        terrain.Advance(step_size);
         sys.DoStepDynamics(step_size);
         t += step_size;
 
