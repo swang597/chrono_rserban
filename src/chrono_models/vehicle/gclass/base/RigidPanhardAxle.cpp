b88d081f9e21cf9b1e2b0e7edc7e3a6b90b8c622
Rainer Gericke <rainer.gericke@mac.com>
2023-10-23 10:08:00 +0200

    JSON classes for G500 model

---
+++ src/chrono_models/vehicle/gclass/base/RigidPanhardAxle.cpp
@@ -0,0 +1,112 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Rainer Gericke, Radu Serban
+// =============================================================================
+//
+// solid panhard axle suspension constructed with data from file.
+//
+// =============================================================================
+
+#include <cstdio>
+
+#include "chrono_models/vehicle/gclass/base/RigidPanhardAxle.h"
+#include "chrono_vehicle/utils/ChUtilsJSON.h"
+
+using namespace rapidjson;
+
+namespace chrono {
+namespace vehicle {
+namespace gclass {
+
+// -----------------------------------------------------------------------------
+// Construct a solid axle suspension using data from the specified JSON
+// file.
+// -----------------------------------------------------------------------------
+RigidPanhardAxle::RigidPanhardAxle(const std::string& filename)
+    : ChRigidPanhardAxle(""), m_springForceCB(NULL), m_shockForceCB(NULL) {
+    Document d;
+    ReadFileJSON(filename, d);
+    if (d.IsNull())
+        return;
+
+    Create(d);
+
+    GetLog() << "Loaded JSON: " << filename.c_str() << "\n";
+}
+
+RigidPanhardAxle::RigidPanhardAxle(const rapidjson::Document& d)
+    : ChRigidPanhardAxle(""), m_springForceCB(NULL), m_shockForceCB(NULL) {
+    Create(d);
+}
+
+RigidPanhardAxle::~RigidPanhardAxle() {}
+
+// -----------------------------------------------------------------------------
+// Worker function for creating a RigidPanhardAxle suspension using data in the
+// specified RapidJSON document.
+// -----------------------------------------------------------------------------
+void RigidPanhardAxle::Create(const rapidjson::Document& d) {
+    // Invoke base class method.
+    ChPart::Create(d);
+
+    if (d.HasMember("Camber Angle (deg)"))
+        m_camber_angle = d["Camber Angle (deg)"].GetDouble() * CH_C_DEG_TO_RAD;
+    else
+        m_camber_angle = 0;
+
+    if (d.HasMember("Toe Angle (deg)"))
+        m_toe_angle = d["Toe Angle (deg)"].GetDouble() * CH_C_DEG_TO_RAD;
+    else
+        m_toe_angle = 0;
+
+    // Read Spindle data
+    assert(d.HasMember("Spindle"));
+    assert(d["Spindle"].IsObject());
+
+    m_spindleMass = d["Spindle"]["Mass"].GetDouble();
+    m_points[SPINDLE] = ReadVectorJSON(d["Spindle"]["COM"]);
+    m_spindleInertia = ReadVectorJSON(d["Spindle"]["Inertia"]);
+    m_spindleRadius = d["Spindle"]["Radius"].GetDouble();
+    m_spindleWidth = d["Spindle"]["Width"].GetDouble();
+
+    // Read Axle Tube data
+    assert(d.HasMember("Axle Tube"));
+    assert(d["Axle Tube"].IsObject());
+
+    m_axleTubeMass = d["Axle Tube"]["Mass"].GetDouble();
+    m_axleTubeCOM = ReadVectorJSON(d["Axle Tube"]["COM"]);
+    m_axleTubeInertia = ReadVectorJSON(d["Axle Tube"]["Inertia"]);
+    m_axleTubeRadius = d["Axle Tube"]["Radius"].GetDouble();
+
+    // Read spring data and create force callback
+    assert(d.HasMember("Spring"));
+    assert(d["Spring"].IsObject());
+    m_points[SPRING_C] = ReadVectorJSON(d["Spring"]["Location Chassis"]);
+    m_points[SPRING_A] = ReadVectorJSON(d["Spring"]["Location Axle"]);
+    m_springForceCB = ReadTSDAFunctorJSON(d["Spring"], m_springRestLength);
+
+    // Read shock data and create force callback
+    assert(d.HasMember("Shock"));
+    assert(d["Shock"].IsObject());
+    m_points[SHOCK_C] = ReadVectorJSON(d["Shock"]["Location Chassis"]);
+    m_points[SHOCK_A] = ReadVectorJSON(d["Shock"]["Location Axle"]);
+    m_shockForceCB = ReadTSDAFunctorJSON(d["Shock"], m_shockRestLength);
+
+    // Read axle inertia
+    assert(d.HasMember("Axle"));
+    assert(d["Axle"].IsObject());
+    m_axleInertia = d["Axle"]["Inertia"].GetDouble();
+}
+
+}  // namespace gclass
+}  // end namespace vehicle
+}  // end namespace chrono
---
+++ src/chrono_models/vehicle/gclass/base/RigidPanhardAxle.h
@@ -0,0 +1,120 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Rainer Gericke, Radu Serban
+// =============================================================================
+//
+// solid Panhard axle suspension constructed with data from file.
+//
+// =============================================================================
+
+#ifndef RIGID_PANHARD_AXLE_H
+#define RIGID_PANHARD_AXLE_H
+
+#include "chrono_vehicle/ChApiVehicle.h"
+#include "chrono_models/vehicle/gclass/base/ChRigidPanhardAxle.h"
+#include "chrono_thirdparty/rapidjson/document.h"
+
+namespace chrono {
+namespace vehicle {
+namespace gclass {
+
+/// @addtogroup vehicle_wheeled_suspension
+/// @{
+
+/// Leaf-spring solid axle suspension constructed with data from file.
+class CH_VEHICLE_API RigidPanhardAxle : public ChRigidPanhardAxle {
+   public:
+    RigidPanhardAxle(const std::string& filename);
+    RigidPanhardAxle(const rapidjson::Document& d);
+    ~RigidPanhardAxle();
+
+   protected:
+    virtual double getCamberAngle() const override { return m_camber_angle; }
+    virtual double getToeAngle() const override { return m_toe_angle; }
+
+    /// Return the center of mass of the axle tube.
+    virtual const ChVector<> getAxleTubeCOM() const override { return m_axleTubeCOM; }
+    /// Return the radius of the spindle body (visualization only).
+    virtual double getSpindleRadius() const override { return m_spindleRadius; }
+
+    /// Return the width of the spindle body (visualization only).
+    virtual double getSpindleWidth() const override { return m_spindleWidth; }
+
+    /// Return the mass of the axle tube body.
+    virtual double getAxleTubeMass() const override { return m_axleTubeMass; }
+    /// Return the mass of the spindle body.
+    virtual double getSpindleMass() const override { return m_spindleMass; }
+
+    /// Return the radius of the axle tube body (visualization only).
+    virtual double getAxleTubeRadius() const override { return m_axleTubeRadius; }
+
+    /// Return the moments of inertia of the axle tube body.
+    virtual const ChVector<>& getAxleTubeInertia() const override { return m_axleTubeInertia; }
+    /// Return the moments of inertia of the spindle body.
+    virtual const ChVector<>& getSpindleInertia() const override { return m_spindleInertia; }
+
+    /// Return the inertia of the axle shaft.
+    virtual double getAxleInertia() const override { return m_axleInertia; }
+
+    /// Return the free (rest) length of the spring element.
+    virtual double getSpringRestLength() const override { return m_springRestLength; }
+
+    /// Return the fre (rest) length of the shock element.
+    virtual double getShockRestLength() const override { return m_shockRestLength; }
+
+    /// Return the functor object for spring force.
+    virtual std::shared_ptr<ChLinkTSDA::ForceFunctor> getSpringForceFunctor() const override { return m_springForceCB; }
+    /// Return the functor object for shock force.
+    virtual std::shared_ptr<ChLinkTSDA::ForceFunctor> getShockForceFunctor() const override { return m_shockForceCB; }
+
+   private:
+    virtual const ChVector<> getLocation(PointId which) override { return m_points[which]; }
+
+    virtual void Create(const rapidjson::Document& d) override;
+
+    std::shared_ptr<ChLinkTSDA::ForceFunctor> m_springForceCB;
+    std::shared_ptr<ChLinkTSDA::ForceFunctor> m_shockForceCB;
+
+    ChVector<> m_points[NUM_POINTS];
+
+    ////double m_damperDegressivityCompression;
+    ////double m_damperDegressivityExpansion;
+
+    double m_camber_angle;
+    double m_toe_angle;
+
+    double m_spindleMass;
+    double m_axleTubeMass;
+    double m_panhardRodMass;
+
+    double m_spindleRadius;
+    double m_spindleWidth;
+    double m_axleTubeRadius;
+    double m_panhardRodRadius;
+
+    double m_springRestLength;
+    double m_shockRestLength;
+    double m_axleInertia;
+
+    ChVector<> m_spindleInertia;
+    ChVector<> m_axleTubeInertia;
+    ChVector<> m_panhardRodInertia;
+    ChVector<> m_axleTubeCOM;
+};
+
+/// @} vehicle_wheeled_suspension
+
+}  // namespace gclass
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
